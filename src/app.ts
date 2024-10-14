890165631fea7252927a445482238149f7419226
Mayank <mayanktiwari1212.mt@gmail.com>
2024-10-15 02:37:16 +0530

    feat: Controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import router from "./router/apiRouter";
+import cors from "cors";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import { ResponseMessage } from "./constant/responseMessage";
 import httpError from "./utils/httpError";
@@ -9,6 +10,7 @@ const app: Application = express();
 
 // Middleware
 app.use(helmet());
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
