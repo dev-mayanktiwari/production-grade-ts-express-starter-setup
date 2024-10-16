2873238e4a5341898a1c34a82ab24e4fe918cc1e
Mayank <mayanktiwari1212.mt@gmail.com>
2024-10-16 19:17:54 +0530

    fix: Error in self controller

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -1,6 +1,3 @@
- 
- 
- 
 import { NextFunction, Request, Response } from "express";
 import httpResponse from "../utils/httpResponse";
 import { EResponseStatusCode } from "../constant/application";
@@ -11,7 +8,7 @@ import moment from "moment";
 export default {
   self: (req: Request, res: Response, next: NextFunction) => {
     try {
-      throw new Error("This is an error");
+     // throw new Error("This is an error");
       httpResponse(req, res, EResponseStatusCode.OK, "Hello World", { name: "John Doe" });
     } catch (error) {
       httpError(next, error, req);
